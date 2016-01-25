d337da3e3a798b58609afe3c4fd7835970323223
root <root@first>
2016-01-25 09:10:04 +0100

    when exporting it creates a folder with the db name , allow to specify the mysql server host

--- mysqlImportTables.sh
+++ mysqlImportTables.sh
@@ -13,13 +13,14 @@ if [ -z "$f" ] || [ -z "$u" ] || [ -z "$p" ] || [ -z "$d" ]  ; then
 fi
 
 
+mysql -C --show-warnings -u root -h$h -p$p -e "SET GLOBAL foreign_key_checks=0"
+mysql -C --show-warnings -u root -h$h -p$p -e "SET GLOBAL max_allowed_packet=1073741824"
 for T in $f; do
     file=$(basename $T)
     table=${file%.*}
     echo "<<<<<  importing  --- $d.$table  >>>>>  "
-    mysql -C --show-warnings -u root -h$h -p$p -e "SET GLOBAL foreign_key_checks=0"
-    mysql -C --show-warnings -u root -h$h -p$p --force $d < "$T"
-    mysql -C --show-warnings -u root -h$h -p$p -e "SET GLOBAL foreign_key_checks=1"
+    mysql -C --show-warnings -u root -h$h -p$p -f --max_allowed_packet=1073741824 $d < "$T" | grep -v "Duplicate entry"
+# | grep -v "Duplicate entry" 
 #    mysqlimport --use-threads=3 --debug-info -u root -h$h -p$p --force -ivL $d "$T"
     	#first need to create the tables so run the first statment from the file which is the CREATE TABLE
 #	sed '/INSERT/Q' $T | mysql -u root -h$h -p$p --force $d 
@@ -27,3 +28,6 @@ for T in $f; do
 #	mysql --local-infile -u root -h$h -p$p --force -C --show-warnings -e  \
 #	"LOAD DATA LOCAL INFILE '$T' INTO TABLE $d.$table;FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'"
 done;
+
+
+mysql -C --show-warnings -u root -h$h -p$p -e "SET GLOBAL foreign_key_checks=1"
